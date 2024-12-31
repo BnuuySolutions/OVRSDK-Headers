b03340805bca0e3715e41d84b1d0ef5389d1c0b0
irlbunny <ItsKaitlyn03@protonmail.com>
2024-12-31 04:02:20 -0500

    Fix function naming

--- Include/OVRSDK/HeadsetPlugin/OVRSDK_IHeadsetInterface.h
+++ Include/OVRSDK/HeadsetPlugin/OVRSDK_IHeadsetInterface.h
@@ -31,65 +31,65 @@ namespace HeadsetPlugin {
 // Used for runtime -> headset plugin interfacing.
 class IHeadsetInterface : public OVRInterface::IUnknown {
 public:
-  virtual void IHeadsetInterface_unk05(char* a2) = 0; // todo
+  virtual void IHeadsetInterface_UnkFunc05(char* a2) = 0; // todo
   virtual void SetPluginMode(ovrPluginMode pluginMode) = 0;
   virtual ovrPluginMode GetPluginMode() = 0;
   virtual void GetPluginName(IOVRString** out_PluginName) = 0;
   virtual void GetRuntimeInterface(IHeadsetToRuntimeInterface2** out_RuntimeInterface) = 0;
-  virtual void IHeadsetInterface_nullsub10() {} // not nullsub on laguna, sets a value in instance.
-  virtual void IHeadsetInterface_nullsub11() {} // not nullsub on laguna, sets a global byte value for a compat info thing.
-  virtual void IHeadsetInterface_unk12() = 0;
+  virtual void IHeadsetInterface_NullsubFunc10() {} // not nullsub on laguna, sets a value in instance.
+  virtual void IHeadsetInterface_NullsubFunc11() {} // not nullsub on laguna, sets a global byte value for a compat info thing.
+  virtual void IHeadsetInterface_UnkFunc12() = 0;
   virtual void GetInterface(u64 iid) = 0;
-  virtual void IHeadsetInterface_nullsub14() {} // not nullsub on laguna
-  virtual void IHeadsetInterface_unk15() = 0;
-  virtual void IHeadsetInterface_unk16() = 0;
-  virtual void IHeadsetInterface_nullsub17() {} // not nullsub on laguna
-  virtual void IHeadsetInterface_unk18() = 0;
-  virtual void IHeadsetInterface_unk19() = 0;
-  virtual void IHeadsetInterface_unk20() = 0;
-  virtual void IHeadsetInterface_unk21() = 0; // vibration related thing?
-  virtual void IHeadsetInterface_unk22() = 0; // sends vibration frequency to controller?
-  virtual void IHeadsetInterface_unk23() = 0; // sends vibration buffer to controller?
-  virtual void IHeadsetInterface_nullsub24() {} // vibrates both controllers?
+  virtual void IHeadsetInterface_NullsubFunc14() {} // not nullsub on laguna
+  virtual void IHeadsetInterface_UnkFunc15() = 0;
+  virtual void IHeadsetInterface_UnkFunc16() = 0;
+  virtual void IHeadsetInterface_NullsubFunc17() {} // not nullsub on laguna
+  virtual void IHeadsetInterface_UnkFunc18() = 0;
+  virtual void IHeadsetInterface_UnkFunc19() = 0;
+  virtual void IHeadsetInterface_UnkFunc20() = 0;
+  virtual void IHeadsetInterface_UnkFunc21() = 0; // vibration related thing?
+  virtual void IHeadsetInterface_UnkFunc22() = 0; // sends vibration frequency to controller?
+  virtual void IHeadsetInterface_UnkFunc23() = 0; // sends vibration buffer to controller?
+  virtual void IHeadsetInterface_NullsubFunc24() {} // vibrates both controllers?
   virtual void WakeFromStandby() = 0;
   virtual void SetVirtualPose(char* deviceName) = 0; // TODO(whatdahopper): Rotation and position.
-  virtual void IHeadsetInterface_nullsub27() {}
-  virtual void IHeadsetInterface_nullsub28() {}
-  virtual void IHeadsetInterface_nullsub29() {} // not nullsub on laguna, something with ipd/screen seperation?
-  virtual void IHeadsetInterface_nullsub30() {} // not nullsub on laguna, sets iad?
-  virtual void IHeadsetInterface_unk31() = 0;
-  virtual void IHeadsetInterface_unk32() = 0;
-  virtual void IHeadsetInterface_unk33() = 0;
-  virtual void IHeadsetInterface_unk34() = 0;
-  virtual void IHeadsetInterface_unk35() = 0;
-  virtual void IHeadsetInterface_unk36() = 0;
-  virtual void IHeadsetInterface_unk37() = 0;
-  virtual void IHeadsetInterface_nullsub38() {}
-  virtual void IHeadsetInterface_nullsub39() {}
+  virtual void IHeadsetInterface_NullsubFunc27() {}
+  virtual void IHeadsetInterface_NullsubFunc28() {}
+  virtual void IHeadsetInterface_NullsubFunc29() {} // not nullsub on laguna, something with ipd/screen seperation?
+  virtual void IHeadsetInterface_NullsubFunc30() {} // not nullsub on laguna, sets iad?
+  virtual void IHeadsetInterface_UnkFunc31() = 0;
+  virtual void IHeadsetInterface_UnkFunc32() = 0;
+  virtual void IHeadsetInterface_UnkFunc33() = 0;
+  virtual void IHeadsetInterface_UnkFunc34() = 0;
+  virtual void IHeadsetInterface_UnkFunc35() = 0;
+  virtual void IHeadsetInterface_UnkFunc36() = 0;
+  virtual void IHeadsetInterface_UnkFunc37() = 0;
+  virtual void IHeadsetInterface_NullsubFunc38() {}
+  virtual void IHeadsetInterface_NullsubFunc39() {}
   virtual u64 GetControllerDeviceType(u32 a2) = 0;
-  virtual void IHeadsetInterface_unk41() = 0;
-  virtual void IHeadsetInterface_unk42() = 0;
-  virtual void IHeadsetInterface_unk43() = 0;
-  virtual void IHeadsetInterface_unk44() = 0;
-  virtual void IHeadsetInterface_unk45() = 0;
-  virtual void IHeadsetInterface_unk46() = 0;
-  virtual void IHeadsetInterface_unk47() = 0;
+  virtual void IHeadsetInterface_UnkFunc41() = 0;
+  virtual void IHeadsetInterface_UnkFunc42() = 0;
+  virtual void IHeadsetInterface_UnkFunc43() = 0;
+  virtual void IHeadsetInterface_UnkFunc44() = 0;
+  virtual void IHeadsetInterface_UnkFunc45() = 0;
+  virtual void IHeadsetInterface_UnkFunc46() = 0;
+  virtual void IHeadsetInterface_UnkFunc47() = 0;
   virtual void GetHMDInfo(ovrHmdInfo* out_HmdInfo) = 0; // todo
-  virtual void IHeadsetInterface_unk49() = 0;
-  virtual void IHeadsetInterface_unk50() = 0;
-  virtual void IHeadsetInterface_unk51() = 0; // latency of something? returns 0 on rift and emulation
-  virtual void IHeadsetInterface_nullsub52() {} // not nullsub on laguna, sets a value in instance.
-  virtual void IHeadsetInterface_unk53() = 0;
-  virtual void IHeadsetInterface_unk54() = 0;
+  virtual void IHeadsetInterface_UnkFunc49() = 0;
+  virtual void IHeadsetInterface_UnkFunc50() = 0;
+  virtual void IHeadsetInterface_UnkFunc51() = 0; // latency of something? returns 0 on rift and emulation
+  virtual void IHeadsetInterface_NullsubFunc52() {} // not nullsub on laguna, sets a value in instance.
+  virtual void IHeadsetInterface_UnkFunc53() = 0;
+  virtual void IHeadsetInterface_UnkFunc54() = 0;
   virtual void SetTrackingCaps(u32 trackingCaps) = 0;
-  virtual void IHeadsetInterface_unk56() = 0;
-  virtual void IHeadsetInterface_unk57() = 0;
-  virtual void IHeadsetInterface_unk58() = 0;
-  virtual void IHeadsetInterface_unk59() = 0;
-  virtual void IHeadsetInterface_unk60() = 0;
-  virtual void IHeadsetInterface_unk61() = 0;
-  virtual void IHeadsetInterface_unk62() = 0;
-  virtual void IHeadsetInterface_unk63() = 0;
+  virtual void IHeadsetInterface_UnkFunc56() = 0;
+  virtual void IHeadsetInterface_UnkFunc57() = 0;
+  virtual void IHeadsetInterface_UnkFunc58() = 0;
+  virtual void IHeadsetInterface_UnkFunc59() = 0;
+  virtual void IHeadsetInterface_UnkFunc60() = 0;
+  virtual void IHeadsetInterface_UnkFunc61() = 0;
+  virtual void IHeadsetInterface_UnkFunc62() = 0;
+  virtual void IHeadsetInterface_UnkFunc63() = 0;
   virtual void SetStandby() = 0;
 };
 
