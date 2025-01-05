d56ffa2583898209d31a5d3e77e4073d776b7924
irlbunny <ItsKaitlyn03@protonmail.com>
2025-01-04 20:25:42 -0500

    Add HMD name and controller serial number

--- Include/OVRSDK/HeadsetPlugin/OVRSDK_IHeadsetInterface.h
+++ Include/OVRSDK/HeadsetPlugin/OVRSDK_IHeadsetInterface.h
@@ -40,29 +40,29 @@ public:
   virtual void IHeadsetInterface_NullsubFunc11() {} // Not nullsub on RiftS, sets a global byte value for compatibility info.
   virtual void IHeadsetInterface_UnkFunc12(u8 a2) = 0; // Firmware update related
   virtual void GetInterface(u64 iid) = 0;
-  virtual void HmdReboot() = 0; // Not nullsub on Rift(S) (sets DeviceManagement)
+  virtual void HmdReboot() = 0; // Not nullsub on Rift(S), sets DeviceManagement HMD mode.
   virtual void IHeadsetInterface_UnkFunc15() = 0;
   virtual u8 IHeadsetInterface_UnkFunc16(u64 a2, u8 a3) = 0; // Controller pairing related
-  virtual void IHeadsetInterface_UnkFunc17() = 0; // Not nullsub on Rift(S) (sets NormalApp)
+  virtual void IHeadsetInterface_UnkFunc17() = 0; // Not nullsub on Rift(S), sets NormalApp HMD mode.
   virtual void IHeadsetInterface_UnkFunc18() = 0;
   virtual void IHeadsetInterface_UnkFunc19() = 0;
   virtual void GetTouchHapticsDesc() = 0; // Closely matches ovr_GetTouchHapticsDesc in CAPI.
   virtual b8 GetControllerVibrationState(s32 deviceType, s32* out_RemainingQueueSpace, s32 out_SamplesQueued) = 0; // True = Success, False = Failure
   virtual b8 SetControllerVibration(s32 deviceType, f32 frequency, f32 amplitude) = 0; // Closely matches ovr_SetControllerVibration in CAPI.
-  virtual b8 SubmitControllerVibration(s32 deviceType, void* samples, s32 samplesCount) = 0;
-  virtual void IHeadsetInterface_NullsubFunc24() {} // vibrates both controllers?
+  virtual b8 SubmitControllerVibration(s32 deviceType, void* samples, s32 samplesCount) = 0; // Unsupported on anything other than Rift.
+  virtual void IHeadsetInterface_UnkFunc24() = 0;
   virtual void WakeFromStandby() = 0;
   virtual void SetVirtualPose(char* deviceName) = 0; // TODO(whatdahopper): Rotation and position.
   virtual void IHeadsetInterface_NullsubFunc27() {}
   virtual void IHeadsetInterface_NullsubFunc28() {}
-  virtual void IHeadsetInterface_NullsubFunc29() {} // not nullsub on laguna, something with ipd/screen seperation?
-  virtual void IHeadsetInterface_NullsubFunc30() {} // not nullsub on laguna, sets iad?
+  virtual void IHeadsetInterface_UnkFunc29() = 0; // Not nullsub on Rift(S), something with IPD/screen seperation?
+  virtual void IHeadsetInterface_UnkFunc30() = 0; // Not nullsub on Rift(S), sets IAD?
   virtual void IHeadsetInterface_UnkFunc31() = 0;
   virtual void IHeadsetInterface_UnkFunc32() = 0;
   virtual void IHeadsetInterface_UnkFunc33() = 0;
   virtual void IHeadsetInterface_UnkFunc34() = 0;
-  virtual void IHeadsetInterface_UnkFunc35() = 0;
-  virtual void IHeadsetInterface_UnkFunc36() = 0;
+  virtual void GetControllerSerialNumber(IOVRString** out_SerialNumber, s32 deviceType) = 0;
+  virtual void GetHmdName(IOVRString** out_HmdName) = 0;
   virtual void IHeadsetInterface_UnkFunc37(IOVRString** out_a2, s32 deviceType) = 0; // Returns HMD serial?
   virtual void IHeadsetInterface_UnkFunc38(IOVRString** out_a2) = 0; // Not nullsub on Rift, outputs JSON.
   virtual void IHeadsetInterface_UnkFunc39(IOVRString** out_a2) = 0; // Not nullsub on Rift, outputs JSON.
@@ -79,18 +79,24 @@ public:
   virtual void IHeadsetInterface_UnkFunc50() = 0;
   virtual void IHeadsetInterface_UnkFunc51() = 0; // latency of something? returns 0 on rift and emulation
   virtual void IHeadsetInterface_NullsubFunc52() {} // not nullsub on laguna, sets a value in instance.
-  virtual void IHeadsetInterface_UnkFunc53() = 0;
+  virtual void IHeadsetInterface_UnkFunc53(u8 a2) = 0;
   virtual void IHeadsetInterface_UnkFunc54() = 0;
   virtual void SetTrackingCaps(u32 trackingCaps) = 0;
   virtual void IHeadsetInterface_UnkFunc56() = 0;
   virtual void IHeadsetInterface_UnkFunc57() = 0;
   virtual void IHeadsetInterface_UnkFunc58() = 0;
-  virtual void IHeadsetInterface_UnkFunc59() = 0;
-  virtual void IHeadsetInterface_UnkFunc60() = 0;
+  virtual void IHeadsetInterface_UnkFunc59(f64 a2) = 0;
+  virtual f64 IHeadsetInterface_UnkFunc60() = 0;
   virtual void IHeadsetInterface_UnkFunc61() = 0;
   virtual void IHeadsetInterface_UnkFunc62() = 0;
   virtual void IHeadsetInterface_UnkFunc63() = 0;
   virtual void SetStandby() = 0;
+  virtual void IHeadsetInterface_UnkFunc65() = 0;
+  virtual void IHeadsetInterface_NullsubFunc66() {}
+  virtual void SignalBoundaryUpdate(const char* a2) = 0;
+  virtual void StartNuxTrackingMode(u32 a2, b8 a3) = 0;
+  virtual void EndNuxTrackingMode() = 0;
+  virtual void SetPerformingSensorPlacement(b8 a2) = 0;
 };
 
 } // HeadsetPlugin
